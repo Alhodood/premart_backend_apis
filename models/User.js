a38a58a13c3b9bf25785e1e08e51735da7663bde
shabanas <shabanaspb.com>
2025-05-10 18:14:54 +0400

    profile screen updated

--- models/User.js
+++ models/User.js
@@ -3,8 +3,7 @@ const bcrypt = require('bcrypt');
 
 const customerAddressDetailsSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true },
-  flatNumber: { type: String, required: true },
+  address: { type: String },
   contact: { type: String, required: true },
   area: { type: String, required: true },
   place: { type: String, required: true },
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { registerUser, loginUser, sendOtp, verifyOtp,resendOtp,updateProfile,updateUserAddress,getAllAddresses,deleteAddress } = require('../controllers/authController');
+const { registerUser, loginUser, sendOtp, verifyOtp,resendOtp,updateProfile,
+    updateUserAddress,getAllAddresses,deleteAddressById,addNewAddress,getProfile } = require('../controllers/authController');
 const { protect } = require('../middleware/authMiddleware');
 const { allowRoles } = require('../middleware/roleMiddleware');
 const adminAuthController = require('../controllers/adminAuthController');
@@ -8,10 +9,14 @@ const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
-router.put('/update-profile/:userId',updateProfile);
-router.put("/update-address/:id/:addressId",updateUserAddress);
-router.get("/get-address/:id",getAllAddresses);
-router.delete("/delet-address/:id/:addressId",deleteAddress);
+router.put('/profile/:userId',updateProfile);
+router.get('/profile/:userId',getProfile);
+
+
+router.get("/address/:id",getAllAddresses);
+router.post("/address/:id",addNewAddress);
+router.put("/address/:id/:addressId",updateUserAddress);
+router.delete("/address/:id/:addressId",deleteAddressById);
 
 
 
