d6fc4e52c4a9c5af82290c94e979b922530dd60a
Ashutosh Patil <patilashu819@gmail.com>
2025-05-19 17:27:40 +0530

    Created Offers & Coupon API's

--- routes/offerCouponRoutes.js
+++ routes/offerCouponRoutes.js
@@ -3,17 +3,21 @@ const express = require('express');
 const router = express.Router();
 const {
   createCoupon, getAllCoupons, deleteCoupon,
-  createOffer, getAllOffers, deleteOffer,updateCoupon,updateOffer
+  createOffer, getAllOffers, deleteOffer,updateCoupon,updateOffer,applyCoupon,checkOfferValidity
 } = require('../controllers/offerCouponController');
 
 router.post('/coupon/:creatorId', createCoupon);
 router.get('/coupon/:creatorId', getAllCoupons);
 router.delete('/coupon/:creatorId/:id', deleteCoupon);
 router.put('/coupon/:id', updateCoupon);
+router.post('/coupon/check', applyCoupon);
+
+//------------------------------
 
 router.post('/offer/:creatorId', createOffer);
 router.get('/offer/:creatorId', getAllOffers);
 router.delete('/offer/:creatorId/:id', deleteOffer);
 router.put('/offer/:id', updateOffer);
+router.post('offer/check', checkOfferValidity);
 
 module.exports = router;
