9d82b9b95f3b4d850d3535a335e317819dda8b61
shabanas <shabanaspb.com>
2025-05-19 12:35:24 +0400

    api created for defult address

--- controllers/authController.js
+++ controllers/authController.js
@@ -303,7 +303,6 @@ exports.updateProfile = async (req, res) => {
   }
 };
 
-
 exports.updateUserAddress = async (req, res) => {
   try {
     const userId = req.params.id;
@@ -319,6 +318,15 @@ exports.updateUserAddress = async (req, res) => {
 
     for (let i = 0; i < user.address.length; i++) {
       if (user.address[i]._id.toString() === addressId) {
+        // If this address is being set to default
+        if (updateData.default === true) {
+          // Set all other addresses' default to false
+          user.address.forEach((addr, index) => {
+            user.address[index].default = false;
+          });
+        }
+
+        // Update the specific address
         user.address[i] = {
           ...user.address[i]._doc,
           ...updateData
@@ -347,6 +355,7 @@ exports.updateUserAddress = async (req, res) => {
     });
   }
 };
+
 
 exports.addNewAddress = async (req, res) => {
   try {
@@ -428,6 +437,35 @@ exports.getAllAddresses = async (req, res) => {
     res.status(500).json({ message: "Failed to fetch addresses", success: false, error: error.message });
   }
 };
+
+exports.getDefultAddress = async (req, res) => {
+  try {
+    const userId = req.params.id;
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found", success: false });
+    }
+
+    const defaultAddress = user.address.find(addr => addr.default === true);
+
+    return res.status(200).json({
+      success: true,
+      message: "Default address fetched successfully",
+      data: defaultAddress ? defaultAddress : [] 
+       // return array or empty list
+    });
+
+  } catch (error) {
+    res.status(500).json({ 
+      message: "Failed to fetch address", 
+      success: false, 
+      data: error.message 
+    });
+  }
+};
+
 
 // exports.deleteAddress = async (req, res) => {
 //   try {
