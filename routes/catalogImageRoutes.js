05116f4f05efa7d2c5af8cfd3d3a699ec0176a19
shabanas <shabanaspb.com>
2025-07-28 12:49:22 +0400

    catalog routes file added

---
+++ routes/catalogImageRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const catalogImageController = require('../controllers/catalogImageController');
+
+// Upload catalog image
+router.post('/upload', catalogImageController.uploadCatalogImage);
+
+// Get catalog images by filters (brand, model, category, subCategory)
+router.get('/', catalogImageController.getCatalogImages);
+router.get('/filter', catalogImageController.getCatalogImagesByNames);
+
+// View catalog image by ID
+router.get('/:id', catalogImageController.viewCatalogImage);
+
+
+// Delete catalog image by ID
+router.delete('/:id', catalogImageController.deleteCatalogImage);
+
+// Update catalog image by ID
+router.put('/:id', catalogImageController.updateCatalogImage);
+
+module.exports = router;
