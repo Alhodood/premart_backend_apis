6a3f1029eb5c02dd73390a40d8b71ff1a30cb872
irshad <irshadalhodood@gmail.com>
2026-01-24 19:00:06 +0400

    WishList Cahnges done

--- routes/wishlistRoutes.js
+++ routes/wishlistRoutes.js
@@ -8,10 +8,13 @@ router.get('/check/:userId', whislistController.checkWishlistStatus);
 // Get wishlist product IDs only (lightweight for status checking)
 router.get('/ids/:userId', whislistController.getWishlistProductIds);
 
+// Get wishlist with all products data populated (ShopProduct + part + shop)
+router.get('/products/:userId', whislistController.getWishListWithProducts);
+
 // Add/Remove from wishlist
 router.post('/:userId', whislistController.addToWishList);
 
-// Get full wishlist with products
+// Get full wishlist (IDs only)
 router.get('/:userId', whislistController.getWishList);
 
 module.exports = router;
