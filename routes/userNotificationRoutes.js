4c79e90f447980e7a10a4236486545c77710a6ac
Ashutosh-Alhodood <ashutosh@alhodood.com>
2026-03-13 14:37:42 +0530

    Delete routes directory

--- routes/userNotificationRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userNotificationController = require('../controllers/userNotificationController');
-
-router.get('/:userId', userNotificationController.getMyUserNotifications);
-router.patch('/:notificationId/read/:userId', userNotificationController.markUserNotificationRead);
-router.patch('/:userId/read-all', userNotificationController.markAllUserNotificationsRead);
-
-module.exports = router;
